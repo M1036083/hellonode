cbe9a4d6e0a19bae197d27d1104a4eb2b3c37d17
M1036083 <Priyanka.Das@mindtree.com>
2018-01-20 21:01:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Node Applicaion Testing my application");
+  response.end("Node Applicaion Testing application");
 });
 
 // listen on localhost:8000
